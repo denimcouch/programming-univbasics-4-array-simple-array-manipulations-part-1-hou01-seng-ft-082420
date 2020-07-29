a8d6fc5c1035adb44a94d838fa410441d066ff84
Alex Mata <alexmata92@ymail.com>
2020-07-29 20:00:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,11 +14,12 @@ def using_pop
 end
 
 def pop_with_args
-  
-  
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  dog_breeds.pop(2)
 end
 
 def using_shift 
+  
   
 end
 
