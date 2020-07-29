ee30444fab8bea62dba911187d5649e232f3eff9
Alex Mata <alexmata92@ymail.com>
2020-07-29 20:28:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,25 +13,21 @@ end
 def using_pop(array)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   continents.pop
-  continents
 end
 
-def pop_with_args(array)
+def pop_with_args
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
   dog_breeds.pop
   dog_breeds.pop
-  dog_breeds
 end
 
 def using_shift 
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   my_favorite_cities.shift
-  my_favorite_cities
 end
 
 def shift_with_args
   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   ice_cream_brands.shift
   ice_cream_brands.shift
-  ice_cream_brands
 end
